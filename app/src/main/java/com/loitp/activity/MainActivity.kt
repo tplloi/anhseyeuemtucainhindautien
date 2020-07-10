7b231acfa4af14a28f6a695839f3d8d3ef625507
v.loitp <v.Loitp@cloudhms.net>
2020-07-10 16:15:12 +0700

    update

--- app/src/main/java/com/loitp/activity/MainActivity.kt
+++ app/src/main/java/com/loitp/activity/MainActivity.kt
@@ -2,12 +2,21 @@ package com.loitp.activity
 
 import android.os.Bundle
 import android.os.Handler
+import android.view.Gravity
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import com.core.base.BaseFontActivity
 import com.core.utilities.LUIUtil
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
 import com.loitp.R
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.view_drawer_behavior_app_bar_default.*
+import kotlinx.android.synthetic.main.view_drawer_behavior_content_default.*
 
-class MainActivity : BaseFontActivity() {
+class MainActivity : BaseFontActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun setFullScreen(): Boolean {
         return false
@@ -24,6 +33,25 @@ class MainActivity : BaseFontActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         LUIUtil.createAdBanner(adView)
+
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawerLayout?.addDrawerListener(toggle)
+        toggle.syncState()
+        navView.setNavigationItemSelectedListener(this)
+        drawerLayout?.useCustomBehavior(Gravity.START)
+        drawerLayout?.useCustomBehavior(Gravity.END)
     }
 
     public override fun onPause() {
@@ -43,14 +71,41 @@ class MainActivity : BaseFontActivity() {
 
     private var doubleBackToExitPressedOnce = false
     override fun onBackPressed() {
-        if (doubleBackToExitPressedOnce) {
+//        if (doubleBackToExitPressedOnce) {
+//            super.onBackPressed()
+//            return
+//        }
+//        this.doubleBackToExitPressedOnce = true
+//        showShort(getString(R.string.press_again_to_exit))
+//        Handler().postDelayed({
+//            doubleBackToExitPressedOnce = false
+//        }, 2000)
+
+        if (drawerLayout?.isDrawerOpen(GravityCompat.START) == true) {
+            drawerLayout?.closeDrawer(GravityCompat.START)
+        } else {
             super.onBackPressed()
-            return
         }
-        this.doubleBackToExitPressedOnce = true
-        showShort(getString(R.string.press_again_to_exit))
-        Handler().postDelayed({
-            doubleBackToExitPressedOnce = false
-        }, 2000)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        drawerLayout?.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_drawer_behavior, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.actionRightDrawer -> {
+                drawerLayout?.openDrawer(GravityCompat.END)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
