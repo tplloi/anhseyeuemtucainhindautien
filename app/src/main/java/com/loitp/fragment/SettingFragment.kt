89df0286517ee0ea4c8b591297737f5de05e2e88
Loitp <v.loitp@cloudhms.net>
2020-12-12 19:42:56 +0700

    update

--- app/src/main/java/com/loitp/fragment/SettingFragment.kt
+++ app/src/main/java/com/loitp/fragment/SettingFragment.kt
@@ -1,5 +1,6 @@
 package com.loitp.fragment
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
@@ -25,27 +26,51 @@ class SettingFragment : BaseFragment() {
         setupViews()
     }
 
+    private var dialog: AlertDialog? = null
     private fun setupViews() {
-        val isDarkTheme = LUIUtil.isDarkTheme()
-        swDarkTheme.isChecked = isDarkTheme
+        fun setData() {
+            val isDarkTheme = LUIUtil.isDarkTheme()
+            swDarkTheme?.isChecked = isDarkTheme
+        }
+
+        setData()
+        var isValid = true
 
         swDarkTheme.setOnCheckedChangeListener { _, isOn ->
-            context?.let { c ->
-                LDialogUtil.showDialog2(
-                        context = c,
-                        title = getString(R.string.warning_vn),
-                        msg = getString(R.string.app_will_be_restart),
-                        button1 = getString(R.string.ok),
-                        button2 = getString(R.string.cancel),
-                        onClickButton1 = {
-                            LUIUtil.setDarkTheme(isDarkTheme = isOn)
-                            val intent = Intent(activity, MainActivity::class.java)
-                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-                            c.startActivity(intent)
-                            LActivityUtil.tranIn(c)
-                        }
-                )
+            activity?.let { a ->
+                if (isValid) {
+                    dialog = LDialogUtil.showDialog2(
+                            context = a,
+                            title = getString(R.string.warning_vn),
+                            msg = getString(R.string.app_will_be_restart),
+                            button1 = getString(R.string.ok),
+                            button2 = getString(R.string.cancel),
+                            onClickButton1 = {
+                                LUIUtil.setDarkTheme(isDarkTheme = isOn)
+                                val intent = Intent(activity, MainActivity::class.java)
+                                a.startActivity(intent)
+                                LActivityUtil.tranIn(a)
+                                a.finishAfterTransition()
+                            },
+                            onClickButton2 = {
+                                isValid = false
+                                setData()
+                            }
+                    )
+                    dialog?.setCancelable(true)
+                    dialog?.setOnCancelListener {
+                        isValid = false
+                        setData()
+                    }
+                } else {
+                    isValid = true
+                }
             }
         }
+    }
+
+    override fun onDestroyView() {
+        dialog?.cancel()
+        super.onDestroyView()
     }
 }
